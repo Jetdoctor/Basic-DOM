d77eac0554ba0a3c4c9f7451695e98fcd8423385
Jetdoctor <7224476+Jetdoctor@users.noreply.github.com>
2022-08-19 07:32:02 +0300

    Mostly JS Dom command tests

---
+++ index.js
@@ -0,0 +1,15 @@
+//document.firstElementChild.lastElementChild.lastElementChild.previousElementSibling.lastElementChild.innerHTML = "Ersel";
+
+document.querySelector("body > ul > li:nth-child(3)").innerHTML="Ersel";
+
+document.getElementsByTagName("li")[1].style.color="blue";
+
+document.getElementsByClassName("btn")[0].innerHTML="Don't Click Me";
+
+document.getElementById("title").innerHTML = "Hello v2";
+
+document.querySelector("li.item a").innerHTML = "Definitely Not Google";
+
+document.querySelector("li a").style.color = "red";
+
+//document.querySelectorAll("#list .item")[2];
