86f8e38163ac4dd53a149ad246345e4a8dbfdebb
Jetdoctor <7224476+Jetdoctor@users.noreply.github.com>
2022-08-25 07:05:52 +0300

    Additional Queries

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //document.firstElementChild.lastElementChild.lastElementChild.previousElementSibling.lastElementChild.innerHTML = "Ersel";
 
-document.querySelector("body > ul > li:nth-child(3)").innerHTML="Ersel";
+document.querySelector("body > ul > li:nth-child(3)").innerHTML = "Ersel";
 
-document.getElementsByTagName("li")[1].style.color="blue";
+document.getElementsByTagName("li")[1].style.color = "blue";
 
-document.getElementsByClassName("btn")[0].innerHTML="Don't Click Me";
+document.getElementsByClassName("btn")[0].innerHTML = "Don't Click Me";
 
 document.getElementById("title").innerHTML = "Hello v2";
 
@@ -12,4 +12,22 @@ document.querySelector("li.item a").innerHTML = "Definitely Not Google";
 
 document.querySelector("li a").style.color = "red";
 
+document.querySelector("button.btn").style.backgroundColor = "yellow";
+
 //document.querySelectorAll("#list .item")[2];
+
+document.querySelector("button").classList.add("invisible");
+
+document.querySelector("button").classList.remove("invisible");
+
+document.querySelector("button").classList.toggle("invisible");
+
+document.querySelector("h1").classList.toggle("huge");
+
+document.querySelector("h1").innerHTML = "<em> Hello v2 </em>"
+
+//document.querySelector("h1").textContent = "Hello v3";
+
+document.querySelector("a").getAttribute("href");
+
+document.querySelector("a").setAttribute("href", "https://www.bing.com");
